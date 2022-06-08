eff4c4dc5be694fbbf11c8ff238c6564448d92ba
Echo <echo.xjtu@gmail.com>
2022-06-08 12:12:58 +0800

    add erase parameter.

--- fw/TB_AG1280/download.sh
+++ fw/TB_AG1280/download.sh
@@ -13,9 +13,13 @@ if [ $# -eq 1 ]; then
 	echo "Program FLASH of AG1280Q48"
 	af.exe -B -X "set blaster_id 0" -X "source -progress 1000 Blink_AG1280_hybrid.prg"
 	;;
+	erase)
+	echo "Erase FLASH of AG1280Q48"
+	af.exe -B -X "set blaster_id 0" -X "set bitgen_usb_speed 3000" -X "exit [catch {erase_flash}]"
+	;;
 	esac
 else
-	echo "Usage $0 [sram|flash]"
+	echo "Usage $0 [sram|flash|erase]"
 fi
 
 
